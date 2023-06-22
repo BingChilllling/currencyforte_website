1149bc06104dd7db3dff122f7d3fc3aa53417626
Prosol1 <prosoldata@gmail.com>
2023-06-22 18:48:37 -0400

    yuh

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState} from 'react';
 import 'tailwindcss/tailwind.css';
 import { Layout } from '@/components';
-import '@/styles/globals.scss'
+import '@/styles/globals.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6469406013541860" crossorigin="anonymous"></script>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
