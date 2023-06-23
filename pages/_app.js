534cf596a641a9c0dc6598f6921f164b2ff94027
Prosol1 <prosoldata@gmail.com>
2023-06-23 04:49:19 -0400

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,45 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useRouter} from 'react';
 import 'tailwindcss/tailwind.css';
 import { Layout } from '@/components';
 import '@/styles/globals.scss';
+import * as gtag from '../lib/gtag';
+import Head from 'next/head';
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Component {...pageProps} />
     </Layout>
   )
