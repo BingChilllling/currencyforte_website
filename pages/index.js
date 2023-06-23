236154a994c01834f1ec0d55e95fce1a5aed0d27
Prosol1 <prosoldata@gmail.com>
2023-06-23 04:12:07 -0400

    forgot

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import { getPosts } from "@/services";
 import FeaturedPosts from "@/sections/FeaturedPosts";
 
 
-
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
