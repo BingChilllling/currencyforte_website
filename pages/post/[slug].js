07f67c5d2d0ff98996828dda7073151071f82847
Prosol1 <prosoldata@gmail.com>
2023-06-24 04:19:16 -0400

    I think I fixed it

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,13 +9,14 @@ const PostDetails = ({ post }) => {
     if(router.isFallback) {
         return <Loader />
     }
-    
+
     return (
         <div className='container mx-auto px-10 mb-9'>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
                 <div className='col-span-1 lg:col-span-8'>
                     <PostDetail post={post}/>
                     <Author author={post.author}/>
+                    <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
                     <CommentsForm slug={post.slug}/>
                     <Comments slug={post.slug}/>
                 </div>
