258d588b596d95830b1c0fc01626f0c1faf943aa
Prosol1 <prosoldata@gmail.com>
2023-07-04 16:09:40 -0400

    adsense go braazyy

--- pages/_app.js
+++ pages/_app.js
@@ -3,46 +3,15 @@ import { useRouter } from 'next/router';
 import 'tailwindcss/tailwind.css';
 import { Layout } from '@/components';
 import '@/styles/globals.scss';
-import * as gtag from '../lib/gtag';
-import Head from 'next/head';
-import Script from 'next/script';
+import { GoogleAdSense } from "nextjs-google-adsense"
 import { Analytics } from '@vercel/analytics/react';
 
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return (
     <Layout>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
+      <GoogleAdSense publisherId="ca-pub-6469406013541860" />
       <Component {...pageProps} />
       <Analytics />
     </Layout>
