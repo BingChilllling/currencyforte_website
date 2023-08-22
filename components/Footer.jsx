084d62b02165fb9280ca557600251a53074d63a7
Jesse Boham <jesseboham21@gmail.com>
2023-08-22 22:54:26 +0200

    iderk

---
+++ components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='container mx-auto bg-blue-700 px-10'> 
+        <div className="w-10/12 mx-auto">
+            <SocialMediaIcons />
+            <div className="md:flex justify-center md:justify-between text-center">
+                <p className="font-playfair font-semibold text-2xl text-yellow-500">
+                    Currencyforte
+                </p>
+                <p className="font-playfair text-md text-yellow-500">
+                    ©2023 Currencyforte. All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
