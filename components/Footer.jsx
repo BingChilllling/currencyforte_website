2b385cf6d122b3d85a2c94c668d38be6af6ead2a
Jesse Boham <jesseboham21@gmail.com>
2023-08-23 13:18:03 +0200

    nkuh jk

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,14 +3,14 @@ import { Socials } from './';
 
 const Footer = () => {
   return (
-    <footer class='footer container mx-auto px-10 '> 
+    <footer className='footer container mx-auto px-10 '> 
         <div className="border-t w-full inline-block border-blue-400 py-8">
             <Socials/>
             <div className="md:flex justify-center md:justify-between text-center">
-                <p className="Playfair Display font-semibold text-2xl text-yellow-500">
+                <p className="font-semibold text-2xl text-yellow-500">
                     Currencyforte
                 </p>
-                <p className="font-playfair text-md text-yellow-500">
+                <p className="text-md text-yellow-500">
                     ©2023 Currencyforte. All Rights Reserved.
                 </p>
             </div>
