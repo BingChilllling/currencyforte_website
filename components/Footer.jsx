da698b6e70b00acce9b25aafa6e0ef5a6b1b3f5a
Jesse Boham <jesseboham21@gmail.com>
2023-08-23 12:31:57 +0200

    kjbhyj

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,11 +3,11 @@ import { Socials } from './';
 
 const Footer = () => {
   return (
-    <div className='container mx-auto px-10 '> 
+    <footer className='container mx-auto px-10 '> 
         <div className="border-t w-full inline-block border-blue-400 py-8">
             <Socials/>
             <div className="md:flex justify-center md:justify-between text-center">
-                <p className="font-playfair font-semibold text-2xl text-yellow-500">
+                <p className="Playfair Display font-semibold text-2xl text-yellow-500">
                     Currencyforte
                 </p>
                 <p className="font-playfair text-md text-yellow-500">
@@ -15,7 +15,7 @@ const Footer = () => {
                 </p>
             </div>
         </div>
-    </div>
+    </footer>
   )
 }
 
